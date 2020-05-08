9e56149fbbf067bc4709d50422576c222a5804de
evgeny-cxx <42057381+evgeny-cxx@users.noreply.github.com>
2020-05-08 15:39:09 +0300

    slyle header and nav, setup formater

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Header from "./Header/Header";
+import Main from "./Main/Main";
+import Conv from "./Conv/Conv";
+import Footer from "./Footer/Footer";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App container">
+        <Header />
+        <h1>Helloo! </h1>
+        <Main />
+        <Conv />
+        <Footer />
+      </div>
+    );
+  }
 }
-
 export default App;
---
+++ src/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import Nav from "../Nav/Nav";
+export default class Footer extends Component {
+  render() {
+    return (
+      <div>
+        <p>Footer</p>
+        <Nav />
+      </div>
+    );
+  }
+}
