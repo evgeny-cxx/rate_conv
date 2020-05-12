ee3693f98f2edb5db1cb9fdabbf6f27e3678cf91
evgeny-cxx <evgeny-cxx@tut.by>
2020-05-12 22:38:14 +0300

    select bank

--- src/App.js
+++ src/App.js
@@ -1,22 +1,56 @@
 import React from "react";
 import "./App.css";
 import Header from "./Header/Header";
-import Main from "./Main/Main";
 import Conv from "./Conv/Conv";
 import Footer from "./Footer/Footer";
+import Belarusbank from "./Banks/Belarusbank";
+import Belagroprombank from "./Banks/Belagroprombank";
+import NacBank from "./Banks/NacBank";
 
 class App extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      select: "Национальный банк",
+    };
   }
 
+  submitForm = (e) => {
+    e.preventDefault();
+  };
+
+  myInput = (e) => {
+    this.setState({ [e.target.name]: e.target.value });
+    console.log({ [e.target.name]: e.target.value });
+  };
+
   render() {
+    let component;
+    if (this.state.select === "Национальный банк") {
+      component = <NacBank />;
+    }
+    if (this.state.select === "Беларусбанк") {
+      component = <Belarusbank />;
+    }
+    if (this.state.select === "БелАгропромбанк") {
+      component = <Belagroprombank />;
+    }
+
     return (
       <div className="App container">
         <Header />
-        <div className=" justify-content-start ">
-          <Main />
-        </div>
+        <form onSubmit={this.submitForm} name="form">
+          <select
+            onChange={this.myInput}
+            name="select"
+            className="custom-select custom-select-lg mb-3"
+          >
+            <option defaultValue="0">Национальный банк</option>
+            <option defaultValue="1">Беларусбанк</option>
+            <option defaultValue="2">БелАгропромбанк</option>
+          </select>
+        </form>
+        <div className="d-flex justify-content-center">{component}</div>
         <Conv />
         <Footer />
       </div>
---
+++ src/Banks/Belagroprombank.js
@@ -0,0 +1,118 @@
+import React, { Component } from "react";
+import "./Belagroprombank.css";
+
+export default class Banks extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoading: true,
+      date: "",
+      curensyRate: {},
+    };
+    this.getRate();
+    this.currensy = ["USD", "EUR", "RUB"];
+  }
+
+  getRate = () => {
+    fetch(
+      "https://cors-anywhere.herokuapp.com/https://belapb.by/ExCardsDaily.php"
+    )
+      .then((response) => {
+        return response.text();
+      })
+      .then((data) => {
+        let parser = new DOMParser(),
+          xmlDoc = parser.parseFromString(data, "text/xml");
+        data = xmlDoc.getElementsByTagName("Currency");
+        console.log("agro:  ", data);
+        this.setState({ isLoading: false });
+        let allRate = {};
+        Object.keys(this.currensy).map((item, index) => {
+          let arr = data[index].textContent.split("\n");
+
+          arr.map((item, index) => {
+            arr[index] = item.trim();
+          });
+          console.log("arr: ", arr);
+          allRate[this.currensy[item]] = [
+            +arr[4],
+            +arr[5],
+            `./flag/${this.currensy[item]}.png`,
+          ];
+        });
+
+        let date = new Date(
+          xmlDoc.getElementsByTagName("DailyExCards")[0].attributes[0].value
+        );
+        date = this.dateFormat(date);
+        console.log("Date  ", date);
+        this.setState({ date: date });
+
+        console.log("allRate", allRate);
+        this.setState({ curensyRate: allRate });
+      });
+  };
+
+  dateFormat = (date) => {
+    let options = {
+      weekday: "long",
+      year: "numeric",
+      month: "long",
+      day: "numeric",
+    };
+    let dateTimeFormat = new Intl.DateTimeFormat("ru-RU", options);
+    return dateTimeFormat.format(date);
+  };
+
+  render() {
+    let container;
+    if (this.state.isLoading) {
+      container = (
+        <div>
+          <div
+            className="spinner-border text-secondary load"
+            style={{ width: "6rem", height: "6rem" }}
+            role="status"
+          >
+            <span className="sr-only">Loading...</span>
+          </div>
+          Loading...
+        </div>
+      );
+    } else {
+      container = (
+        <table className="list-unstyled">
+          <tr>
+            <td>Валюта</td>
+            <td>Продажа</td>
+            <td>Покупка</td>
+          </tr>
+
+          {Object.keys(this.state.curensyRate).map((elem, index) => (
+            <tr>
+              <td className="media-body" key={index}>
+                <img
+                  src={this.state.curensyRate[elem][2]}
+                  className="mr-3"
+                  width="20"
+                  height="16"
+                  alt={[elem]}
+                />
+                {[elem]} :
+              </td>
+              <td>{this.state.curensyRate[elem][0].toFixed(2)} BYN. </td>
+              <td>{this.state.curensyRate[elem][1].toFixed(2)} BYN. </td>
+            </tr>
+          ))}
+        </table>
+      );
+    }
+    return (
+      <div className="Rate">
+        <p>Курсы валют Белагропромбанк на </p>
+        <p className="Date">{this.state.date}</p>
+        <div>{container}</div>
+      </div>
+    );
+  }
+}
