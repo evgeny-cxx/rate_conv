e9a0a0eeb6dc625a4cd8fd245320452d4e4cd79c
evgeny-cxx <42057381+evgeny-cxx@users.noreply.github.com>
2020-06-12 17:56:29 +0300

    price customs

--- src/App.js
+++ src/App.js
@@ -2,51 +2,119 @@ import React from "react";
 import "./App.css";
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
-import { Route, Switch, Redirect } from "react-router-dom";
-import Main from "./Main/Main";
 import Customs from "./Customs/Customs";
+import Belarusbank from "./Banks/Belarusbank";
+import Belagroprombank from "./Banks/Belagroprombank";
+import NacBank from "./Banks/NacBank";
+import Alfabank from "./Banks/Alfabank";
+import Conv from "./Conv/Conv";
+import { Route, Switch, Redirect } from "react-router-dom";
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      currentRate: {
-        USD: [1, 2.3748, "Доллар США", "../flag/USD.png"],
-        EUR: [1, 2.7006, "Евро", "../flag/EUR.png"],
-        RUB: [100, 3.4386, "Российских рублей", "../flag/RUB.png"],
-      },
+      select: "Национальный банк",
+      currentRate: {},
     };
   }
 
-  updateDataApp = (value) => {
-    this.setState({ currentyRate: value });
+  updateData = (value) => {
+    this.setState({ currentRate: value });
     console.log("app", this.state.currentRate);
   };
   noData = () => {
     return null;
   };
 
+  submitForm = (e) => {
+    e.preventDefault();
+  };
+
+  myInput = (e) => {
+    this.setState({ [e.target.name]: e.target.value });
+    console.log({ [e.target.name]: e.target.value });
+  };
+
   render() {
     const { currentRate } = this.state;
+    let selectForm = (
+      <form onSubmit={this.submitForm} name="form">
+        <select
+          onChange={this.myInput}
+          name="select"
+          className="custom-select custom-select-lg mb-3"
+        >
+          <option defaultValue="0">Национальный банк</option>
+          <option defaultValue="1">Беларусбанк</option>
+          <option defaultValue="2">Белагропромбанк</option>
+          <option defaultValue="3">Альфабанк</option>
+          <option defaultValue="4">Все банки</option>
+        </select>
+      </form>
+    );
+    let component;
+    if (this.state.select === "Национальный банк") {
+      component = <NacBank updateData={this.updateData} />;
+    }
+    if (this.state.select === "Беларусбанк") {
+      component = <Belarusbank updateData={this.updateData} />;
+    }
+    if (this.state.select === "Белагропромбанк") {
+      component = <Belagroprombank updateData={this.updateData} />;
+    }
+    if (this.state.select === "Альфабанк") {
+      component = <Alfabank updateData={this.updateData} />;
+    }
+    if (this.state.select === "Все банки") {
+      component = (
+        <div>
+          <div className="row">
+            <div className="mx-auto col-sm-6">
+              <NacBank updateData={this.updateData} />
+            </div>
+            <div className="mx-auto col-sm-6">
+              <Belarusbank updateData={this.noData} />
+            </div>
+          </div>
+
+          <div className="row">
+            <div className="mx-auto col-sm-6">
+              <Belagroprombank updateData={this.noData} />
+            </div>
+            <div className="mx-auto col-sm-6">
+              <Alfabank updateData={this.noData} />
+            </div>
+          </div>
+        </div>
+      );
+    }
     return (
       <div className="container">
         <header>
           <Header />
         </header>
         <main className="page">
-          <Switch>
-            <Route
-              exact
-              path="/"
-              render={(props) => <Main updateDataApp={this.updateDataApp} />}
-            />
-            <Route
-              path="/Customs"
-              render={(props) => <Customs currentRate={currentRate} />}
-            />
-          </Switch>
+          <div className="row d-flex justify-content-around">
+            <div className="d-flex align-self-center ">{component}</div>
+            <div className="align-self-center">
+              <div className=" mt-5">{selectForm}</div>
 
-          {/* <Redirect to="/" /> */}
+              <Switch>
+                <Route
+                  exact
+                  path="/"
+                  render={(props) => <Conv currentRate={currentRate} />}
+                />
+                <Route
+                  path="/Customs"
+                  render={(props) => <Customs currentRate={currentRate} />}
+                />
+              </Switch>
+
+              {/* <Redirect to="/" /> */}
+            </div>
+          </div>
         </main>
         <footer className="container fixed-bottom">
           <Footer />
--- src/Customs/Customs.js
+++ src/Customs/Customs.js
@@ -5,7 +5,15 @@ class Customs extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      sale: null,
+      priceUSD: 0,
+      priceBY: 0,
+      priceEUR: 0,
+      percentUSD: 0,
+      percentBY: 0,
+      percentEUR: 0,
+      fullPriceUSD: 0,
+      fullPriceBY: 0,
+      fullPriceEUR: 0,
     };
   }
 
@@ -20,22 +28,41 @@ class Customs extends React.Component {
 
   customs = () => {
     const { rate } = this.props.currentRate;
+    console.log("rate", rate);
   };
   render() {
-    // const { rate } = this.props.currentRate;
+    let rate = this.state;
+    let content = (
+      <form>
+        {Object.keys(rate).map((item) => (
+          <div className="form-group row" key={item}>
+            <label
+              className="col-sm-2 col-form-label col-form-label-lg"
+              htmlFor={item}
+            >
+              {item}
+            </label>
+            <div className="col col-lg-10">
+              <input
+                className="form-control form-control-lg"
+                onChange={this.myInput}
+                type="text"
+                autoComplete="off"
+                name={item}
+                id={item}
+                value={this.state[item]}
+              />
+            </div>
+          </div>
+        ))}
+      </form>
+    );
     return (
       <div>
-        <form onSubmit={this.submitForm} name="form">
-          <div>
-            <input
-              onChange={this.myInput}
-              type="text"
-              name="sale"
-              placeholder="Введите цену"
-            />
-          </div>
-        </form>
-        {/* <div>{this.props.currentRate}</div> */}
+        <div>{content}</div>
+        <div>
+          <h3>{this.messageError}</h3>
+        </div>
       </div>
     );
   }
