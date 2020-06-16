f290dcd518fe315178e9ce17ac56a4de6acdabeb
evgeny-cxx <42057381+evgeny-cxx@users.noreply.github.com>
2020-06-16 11:40:44 +0300

    active link

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -1,13 +1,22 @@
 import React, { Component } from "react";
-import Nav from "../Nav/Nav";
 import "./Footer.css";
+
+let title = "Курсы валют";
+let contacts = ["Copyright by cxx_BY", "+375298174777", "evgeny.cxx@gmail.com"];
 
 export default class Footer extends Component {
   render() {
     return (
-      <footer className="Footer">
-        <p>Footer</p>
-        <Nav />
+      <footer className="Footer App section" style={{ background: "#2d3246" }}>
+        <p>{title}</p>
+        {/* <p className="d-flex bd-highlight"> Copyright by cxx_BY</p> */}
+        <ul className="row d-flex">
+          {contacts.map((elem, index) => (
+            <li className="p-2 flex-fill bd-highlight" key={index}>
+              {elem}
+            </li>
+          ))}
+        </ul>
       </footer>
     );
   }
