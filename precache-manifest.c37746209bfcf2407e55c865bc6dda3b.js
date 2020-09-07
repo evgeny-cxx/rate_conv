6e4a9dffece206090cccf5c1bf7e7cb5d45e8f27
evgeny-cxx <evgeny-cxx@tut.by>
2020-09-07 18:45:49 +0300

    Updates

---
+++ precache-manifest.c37746209bfcf2407e55c865bc6dda3b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d47ab09d39a5a63e589c1b7d703b0d47",
+    "url": "/rate/index.html"
+  },
+  {
+    "revision": "e6d940801b4e16727058",
+    "url": "/rate/static/css/2.5b44f0a0.chunk.css"
+  },
+  {
+    "revision": "e2a13d273d834f083b39",
+    "url": "/rate/static/css/main.57ee7fcb.chunk.css"
+  },
+  {
+    "revision": "e6d940801b4e16727058",
+    "url": "/rate/static/js/2.c229953e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/rate/static/js/2.c229953e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e2a13d273d834f083b39",
+    "url": "/rate/static/js/main.a928c425.chunk.js"
+  },
+  {
+    "revision": "c94d2bc9f546b1c3807a",
+    "url": "/rate/static/js/runtime-main.be4ea6a2.js"
+  }
+]);
